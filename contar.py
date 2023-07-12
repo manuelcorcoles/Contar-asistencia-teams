6147746609b2475a0eba9cbe728a8a8aabc13cd1
manuelcorcoles <91339576+manuelcorcoles@users.noreply.github.com>
2023-07-12 19:01:32 +0200

    Update contar.py

--- contar.py
+++ contar.py
@@ -3,7 +3,7 @@ import pandas as pd
 from datetime import datetime, timedelta
 
 # Ruta de la carpeta que contiene los archivos CSV
-carpeta = '/home/manu/Contar-asistencia-teams/csv'
+carpeta = '/home/manu/Contar-asistencia-teams/csv/csv_miercoles'
 
 # Crear un diccionario para almacenar el recuento de cada persona
 recuento_personas_tiempo = {}
@@ -17,36 +17,34 @@ for archivo in os.listdir(carpeta):
     if archivo.endswith('.csv'):  # Asegurarse de que solo se procesen archivos CSV
         ruta_archivo = os.path.join(carpeta, archivo)
         
-        # Incrementar el contador de Excels recorridos
-        excels_recorridos += 1
-        
         # Leer el archivo CSV en un DataFrame
         df = pd.read_csv(ruta_archivo)
         
-        # Verificar la existencia de las columnas 'SessionStartTime' y 'SessionEndTime'
-        if 'SessionStartTime' in df.columns and 'SessionEndTime' in df.columns:
+        # Incrementar el contador de Excels recorridos
+        excels_recorridos += 1
+        
+        # Verificar la existencia de las columnas necesarias
+        if 'ParticipantDisplayName' in df.columns and 'SessionStartTime' in df.columns and 'SessionEndTime' in df.columns:
             # Convertir las columnas de fecha y hora a objetos datetime
             df['SessionStartTime'] = pd.to_datetime(df['SessionStartTime'])
             df['SessionEndTime'] = pd.to_datetime(df['SessionEndTime'])
             
-            # Calcular la diferencia de tiempo entre 'SessionStartTime' y 'SessionEndTime'
-            df['TimeDifference'] = df['SessionEndTime'] - df['SessionStartTime']
+            # Calcular la duración de cada sesión
+            df['Duration'] = df['SessionEndTime'] - df['SessionStartTime']
             
-            # Filtrar las filas con una diferencia de tiempo mayor a 1.5 horas (90 minutos)
-            df_filtrado = df[df['TimeDifference'] > timedelta(minutes=90)]
+            # Agrupar por persona y sumar la duración de las sesiones
+            group_by_persona = df.groupby('ParticipantDisplayName')
+            duracion_total = group_by_persona['Duration'].sum()
             
-            # Obtener la columna 'ParticipantDisplayName' del DataFrame filtrado
-            column_name = 'ParticipantDisplayName'
-            if column_name in df_filtrado.columns:
-                # Contar la aparición de cada persona en el DataFrame filtrado
-                conteo_tiempo = df_filtrado[column_name].value_counts()
-                
-                # Actualizar el recuento global de cada persona
-                for persona, count in conteo_tiempo.items():
-                    recuento_personas_tiempo[persona] = recuento_personas_tiempo.get(persona, 0) + count
+            # Filtrar por duración mayor o igual a 90 minutos
+            duracion_mas_90 = duracion_total[duracion_total >= timedelta(minutes=90)]
+            
+            # Contar las apariciones de más de 90 minutos por persona
+            for persona, duracion in duracion_mas_90.items():
+                recuento_personas_tiempo[persona] = recuento_personas_tiempo.get(persona, 0) + 1
         
-        # Contar la aparición de cada persona sin tener en cuenta el tiempo
-        conteo_total = df[column_name].value_counts()
+        # Contar las apariciones totales por persona
+        conteo_total = df['ParticipantDisplayName'].value_counts()
         
         # Actualizar el recuento global de cada persona
         for persona, count in conteo_total.items():
@@ -56,17 +54,12 @@ for archivo in os.listdir(carpeta):
 personas_tiempo_ordenadas = sorted(recuento_personas_tiempo.items(), key=lambda x: x[1], reverse=True)
 personas_total_ordenadas = sorted(recuento_personas_total.items(), key=lambda x: x[1], reverse=True)
 
-# Imprimir el número de Excels recorridos
-print(f'Número de Excels recorridos: {excels_recorridos}')
-
-# Imprimir el número de apariciones de más de 90 minutos y de menos para cada persona
-for persona, count_tiempo in personas_tiempo_ordenadas:
-    count_total = recuento_personas_total.get(persona, 0)
-    print(f'{persona}: {count_tiempo}/{count_total}')
-
-# Imprimir el número total de apariciones sin tener en cuenta el tiempo
-print('Total de apariciones sin tener en cuenta el tiempo:')
-for persona, count in personas_total_ordenadas:
-    # Evitar imprimir las apariciones nuevamente si ya se imprimieron en el bloque anterior
-    if persona not in recuento_personas_tiempo:
-        print(f'{persona}: {count} veces')
+# Guardar la salida en un archivo de texto
+with open("GrupoMiercoles.txt", "w") as archivo_salida:
+    # Escribir el número de Excels recorridos
+    archivo_salida.write(f'Número de Excels recorridos de la clase del lunes: {excels_recorridos}\n\n')
+    
+    # Escribir el número de apariciones de más de 90 minutos y de menos para cada persona
+    for persona, count_tiempo in personas_tiempo_ordenadas:
+        count_total = recuento_personas_total.get(persona, 0)
+        archivo_salida.write(f'{persona}: {count_tiempo}\n')
